8169e4bc9b4bda7a744e805dd53617203f4bd049
Matthew Cline <clinemattw@gmail.com>
2019-03-26 23:44:03 -0500

    added semantic ui icons

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,6 @@
-function printReverse(arr) {
-	for (var i = arr.length - 1; i >= 0; i--) {
-		console.log(arr[i]);
-	}
-}
+console.log("Connected");
 
-function isUniform(arr) {
-	if (arr.length === 0) {
-		return false;
-	}
-	var ref = arr[0];
-	for (var i = 1; i < arr.length; i++) {
-		if (arr[i] !== ref) {
-			return false;
-		}
-	}
-	return true;
-}
-
-function sumArray(arr) {
-	sum = 0;
-	for (var i = 0; i < arr.length; i++) {
-		sum += arr[i];
-	}
-	return sum;
-}
-
-function maxVal(arr) {
-	if (arr.length === 0) {
-		return null;
-	}
-	max = arr[0];
-	for (var i = 1; i < arr.length; i++) {
-		var val = arr[i];
-		if (val > max) {
-			max = val;
-		}	
-	}
-	return max;
-}
+// $("#selfPortrait").on("click", function() {
+// 	console.log("clicked!");
+// 	$(this).transition('jiggle');
+// });
