fbba35c90ec37c3fecc359ccb2f8783a45908a68
Matthew Cline <clinemattw@gmail.com>
2019-04-04 00:18:45 -0500

    update navbar with white background upon scroll, new logo color upon scroll, bottom border upon hover, and semantic ui icons next to projects and travel links

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
-console.log("Connected");
+// Navbar scrolling feature
+// Adapted from https://stackoverflow.com/questions/29646622/set-bootstrap-navbar-transparency-on-scroll
+function checkScroll(){
+    var scrollPoint = $('.navbar').height() * 2; //The point where the navbar changes in px
+    if($(window).scrollTop() > scrollPoint) {
+        $('.navbar').addClass("scrolled");
+        $('#mc-logo').attr("src", "imgs/mc_scrolled.png");
+    } else{
+        $('.navbar').removeClass("scrolled");
+        $('#mc-logo').attr("src", "imgs/mc.png");
+    }
+}
 
-// $("#selfPortrait").on("click", function() {
-// 	console.log("clicked!");
-// 	$(this).transition('jiggle');
-// });
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
